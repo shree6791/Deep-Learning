e37de6b10c0ab16194a156499a1deb2d5b048d55
shree6791 <shree6791@yahoo.com>
2017-01-08 22:37:20 -0500

    DL Projects

--- ANN/src/model/Ann.py
+++ ANN/src/model/Ann.py
@@ -34,20 +34,24 @@ with tf.name_scope('input'):
     x = tf.placeholder(tf.float32, [None, 784], name='x-input')
     y = tf.placeholder(tf.float32, [None, 10], name='y-input')
 
+# Display Image
 with tf.name_scope('input_reshape'):
     image_shaped_input = tf.reshape(x, [-1, 28, 28, 1])
     tf.summary.image('input', image_shaped_input, 10)
 
+# 1st Hidden Layer
 hidden1 = nw.nn_layer(x, 784, 500, 'layer1')
 
+# Dropout
 with tf.name_scope('dropout'):
     keep_prob = tf.placeholder(tf.float32)
     dropped = tf.nn.dropout(hidden1, keep_prob)
 
+# Output Layer
 y_ = nw.nn_layer(dropped, 500, 10, 'layer2', act=tf.identity)
 
+# Find Cross Entropy
 with tf.name_scope('cross_entropy'):
-
     with tf.name_scope('total'):
         cross_entropy = tf.reduce_mean(
             tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=y_))
@@ -57,6 +61,7 @@ with tf.name_scope('train'):
     train_step = tf.train.AdamOptimizer(learning_rate).minimize(
         cross_entropy)
 
+# Accuracy of Model
 with tf.name_scope('accuracy'):
 
     with tf.name_scope('correct_prediction'):
@@ -65,34 +70,37 @@ with tf.name_scope('accuracy'):
     with tf.name_scope('accuracy'):
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
+# Log the Output
 tf.summary.scalar('cross_entropy', cross_entropy)
 tf.summary.scalar('accuracy', accuracy)
 
 # Merge All Summaries
-merged = tf.summary.merge_all()
+summary_op = tf.summary.merge_all()
 
+# Launch Graph In Session
 with tf.Session() as sess:
 
     tf.global_variables_initializer().run()
 
+    # Initialize Log Paths
     train_writer = tf.summary.FileWriter(log_dir + '/train', sess.graph)
     test_writer = tf.summary.FileWriter(log_dir + '/test')
 
     for i in range(max_steps):
 
-        if i % 10 == 0:
+        # Print Test Accuracy and Log Test Parameters
+        if i % 50 == 0:
 
             summary, acc = sess.run(
-                [merged, accuracy], feed_dict=nw.feed_dict(dropout, keep_prob, mnist, False, x, y))
+                [summary_op, accuracy], feed_dict=nw.feed_dict(dropout, keep_prob, mnist, False, x, y))
 
+            print('Test Accuracy @ Step %s: %s' % (i, acc))
             test_writer.add_summary(summary, i)
-            print('Accuracy at step %s: %s' % (i, acc))
 
-        else:
-            summary, _ = sess.run(
-                [merged, train_step], feed_dict=nw.feed_dict(dropout, keep_prob, mnist, True, x, y))
-
-            train_writer.add_summary(summary, i)
+        # Log Train Parameters
+        summary, _ = sess.run(
+            [summary_op, train_step], feed_dict=nw.feed_dict(dropout, keep_prob, mnist, True, x, y))
+        train_writer.add_summary(summary, i)
 
     train_writer.close()
     test_writer.close()
