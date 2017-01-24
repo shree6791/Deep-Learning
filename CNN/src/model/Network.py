957d03fa60f843878a90a7376bed9e3ceefc5342
shree6791 <shree6791@yahoo.com>
2017-01-24 02:24:15 -0500

    DL Projects 2

--- CNN/src/model/Network.py
+++ CNN/src/model/Network.py
@@ -10,9 +10,14 @@ class Network(object):
       initial = tf.constant(0.1, shape=shape)
       return tf.Variable(initial)
       
-    def conv2d(self,x, W):
+    def conv2d_Same(self,x, W):
       return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
     
-    def max_pool_2x2(self, x):
-      return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
-                            strides=[1, 2, 2, 1], padding='SAME')
+    def max_pool_2x2_Same(self, x):
+      return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+      
+    def conv2d_Valid(self,x, W):
+      return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='VALID')
+    
+    def max_pool_2x2_Valid(self, x):
+      return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],strides=[1, 2, 2, 1], padding='VALID')
